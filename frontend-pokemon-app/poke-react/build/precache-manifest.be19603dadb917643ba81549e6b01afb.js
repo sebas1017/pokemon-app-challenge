7bdec97b762bb3dd07c2e595bd0c9fa6269b77ff
sebastianhenao <sebastianhenao@habi.co>
2023-03-23 10:52:34 -0500

    build deploy

---
+++ frontend-pokemon-app/poke-react/build/precache-manifest.be19603dadb917643ba81549e6b01afb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fb687033f65070ef4101a01a85cfd52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e16fefbb830edbb3298",
+    "url": "/static/css/main.4c4619ce.chunk.css"
+  },
+  {
+    "revision": "5d85b76d38376ba6bbeb",
+    "url": "/static/js/2.dc26f645.chunk.js"
+  },
+  {
+    "revision": "1e16fefbb830edbb3298",
+    "url": "/static/js/main.61af196e.chunk.js"
+  },
+  {
+    "revision": "040cb37eb74ee05bf1cf",
+    "url": "/static/js/runtime-main.c8786d92.js"
+  }
+]);
